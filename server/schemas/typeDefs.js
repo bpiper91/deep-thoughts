cd5dea1876d831a0972fbfcbdc45c131fa6f7b53
bpiper91 <bpiper91@gmail.com>
2022-10-17 22:11:19 -0400

    added addThought, addReaction, and addFriend mutations, with auth token headers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -33,11 +33,15 @@ const typeDefs = gql`
     thought(_id: ID!): Thought
     users: [User]
     user(username: String): User
+    me: User
   }
 
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
   }
 
   type Auth {
